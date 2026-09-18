06094b0ff6a6c89a9fc9b2d8768c608f229247e0
teng6 <michaelangelosamson91@gmail.com>
2026-09-18 18:29:53 +0800

    PHASE 0: Alert, AlertUser, Outbreak

---
+++ app/Models/Outbreak.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\StatusEnum;
+use Illuminate\Database\Eloquent\Model;
+
+class Outbreak extends Model
+{
+    protected function casts(): array{
+        return [
+            'status' => StatusEnum::class,
+        ];
+    }
+}
