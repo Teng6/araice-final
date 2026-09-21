3bc826080b48c7183612cdc2a47a5513ec8ff4e5
teng6 <michaelangelosamson91@gmail.com>
2026-09-21 21:42:49 +0800

    start phase 1

--- app/Models/Scan.php
+++ app/Models/Scan.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Scan extends Model
 {
+    protected $fillable = ['farmer_id', 'uploaded_by', 'scan_type', 'disease_id', 'variety_id',
+        'outbreak_id', 'image_url', 'confidence_score', 'raw_predictions', 'status', 'gps_lat',
+        'gps_long', 'scan_date',
+    ];
     protected function casts(): array
     {
         return [
